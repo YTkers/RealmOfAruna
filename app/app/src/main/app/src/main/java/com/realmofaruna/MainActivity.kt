bcf60ce37be0ddae979c98f39aebb5c00b3689c3
YTkers <56787602+YTkers@users.noreply.github.com>
2026-08-19 17:05:09 +0700

    MainActivity.kt

---
+++ app/app/src/main/app/src/main/java/com/realmofaruna/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.realmofaruna
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Realm Of Aruna"
+        textView.textSize = 24f
+
+        setContentView(textView)
+    }
+}
