a51f067b38a9ea60ed001915530131ad7e294007
YTkers <56787602+YTkers@users.noreply.github.com>
2026-08-19 19:14:42 +0700

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,4 +13,15 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
