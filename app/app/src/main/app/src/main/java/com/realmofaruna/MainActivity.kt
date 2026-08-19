d1c8c5e3b238cf69aa79761d9164446adafb9908
YTkers <56787602+YTkers@users.noreply.github.com>
2026-08-19 18:42:36 +0700

    MainActivity.kt

--- app/app/src/main/app/src/main/java/com/realmofaruna/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/realmofaruna/MainActivity.kt
@@ -2,34 +2,22 @@ package com.realmofaruna
 
 import android.app.Activity
 import android.os.Bundle
-import android.webkit.WebView
+import android.graphics.Color
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        val text = TextView(this)
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
+        text.text = "REALM OF ARUNA\n\nAPK BERHASIL BERJALAN"
+        text.textSize = 24f
+        text.setTextColor(Color.WHITE)
+        text.setBackgroundColor(Color.rgb(16, 19, 26))
+        text.gravity = android.view.Gravity.CENTER
 
-        webView.loadDataWithBaseURL(
-            null,
-            """
-            <!DOCTYPE html>
-            <html>
-            <body style="background:#10131a;color:white;text-align:center;padding-top:100px">
-                <h1>Realm Of Aruna</h1>
-                <p>WebView berhasil berjalan!</p>
-            </body>
-            </html>
-            """.trimIndent(),
-            "text/html",
-            "UTF-8",
-            null
-        )
-
-        setContentView(webView)
+        setContentView(text)
     }
 }
