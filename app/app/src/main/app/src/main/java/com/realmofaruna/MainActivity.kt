a36dad4c6a03a78ba3ea7af52468394367e0fd71
YTkers <56787602+YTkers@users.noreply.github.com>
2026-08-19 18:37:51 +0700

    MainActivity.kt

--- app/app/src/main/app/src/main/java/com/realmofaruna/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/realmofaruna/MainActivity.kt
@@ -2,88 +2,34 @@ package com.realmofaruna
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.webkit.ConsoleMessage
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Toast
 
 class MainActivity : Activity() {
-
-    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
-            webView = WebView(this)
+        val webView = WebView(this)
 
-            webView.settings.apply {
-                javaScriptEnabled = true
-                domStorageEnabled = true
-                databaseEnabled = true
-                allowFileAccess = true
-                allowContentAccess = true
-                builtInZoomControls = false
-                displayZoomControls = false
-            }
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
 
-            webView.webViewClient = object : WebViewClient() {
-                override fun onReceivedError(
-                    view: WebView?,
-                    request: WebResourceRequest?,
-                    error: WebResourceError?
-                ) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Game gagal dimuat",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+        webView.loadDataWithBaseURL(
+            null,
+            """
+            <!DOCTYPE html>
+            <html>
+            <body style="background:#10131a;color:white;text-align:center;padding-top:100px">
+                <h1>Realm Of Aruna</h1>
+                <p>WebView berhasil berjalan!</p>
+            </body>
+            </html>
+            """.trimIndent(),
+            "text/html",
+            "UTF-8",
+            null
+        )
 
-                override fun onRenderProcessGone(
-                    view: WebView?,
-                    detail: RenderProcessGoneDetail?
-                ): Boolean {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "WebView game crash",
-                        Toast.LENGTH_LONG
-                    ).show()
-                    return true
-                }
-            }
-
-            webView.webChromeClient = object : WebChromeClient() {
-                override fun onConsoleMessage(message: ConsoleMessage): Boolean {
-                    android.util.Log.d(
-                        "RealmOfAruna",
-                        message.message() + " @" + message.lineNumber()
-                    )
-                    return true
-                }
-            }
-
-            setContentView(webView)
-
-            webView.loadUrl("file:///android_asset/index.html")
-
-        } catch (e: Exception) {
-            Toast.makeText(
-                this,
-                "ERROR: ${e.message}",
-                Toast.LENGTH_LONG
-            ).show()
-        }
-    }
-
-    override fun onDestroy() {
-        if (::webView.isInitialized) {
-            webView.destroy()
-        }
-        super.onDestroy()
+        setContentView(webView)
     }
 }
